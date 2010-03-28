fcb28f34490d152a222865008c73f64ac81f19db
Weldys Santos <weldyss@gmail.com>
2010-03-27 23:27:49 -0300

    adicionada a regra de negócios

--- grails-app/domain/Queixa.groovy
+++ grails-app/domain/Queixa.groovy
@@ -4,4 +4,12 @@ class Queixa {
 	Date data_queixa
 	String queixa
 	Integer status
+	
+	static constraints = {
+		tipo(inList:["Alimentar", "Animal"])
+		nome_cidadao(blank:false, nullable:false, maxSize:100)
+		data_queixa(max:new Date(), nullable:false)
+		status(inList:["Aberto", "Fechado"])
+		queixa(blank:false, maxSize:1000)
+	}
 }
